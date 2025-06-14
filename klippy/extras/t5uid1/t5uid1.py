6bae01ee4f6380262181123e7a538b7db480b9bb
Desterly <desterly@stressfactor.net>
2025-06-13 22:25:37 -0400

    t5uid1.py - update probed location count
    This change prevents crashing when attempting to run a manual probe and
    allows the checkmark on the display to properly show current completed
    probe points.

--- klippy/extras/t5uid1/t5uid1.py
+++ klippy/extras/t5uid1/t5uid1.py
@@ -765,7 +765,7 @@ class T5UID1:
     def probed_matrix(self):
         if self.bed_mesh is None:
             return 0
-        count = len(self.bed_mesh.bmc.probe_helper.results)
+        count = len(self.probe.probe_session.results)
         points_map = [ 0,  1,  2,  3,  4,
                        9,  8,  7,  6,  5,
                       10, 11, 12, 13, 14,
